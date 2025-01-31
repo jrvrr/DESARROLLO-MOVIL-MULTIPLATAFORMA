87181e5d2efb8d456f587047b82e12308053396a
jrvrr <moralesriveraj10@gmail>
2025-01-30 23:17:52 -0600

    agregación del menu

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  standalone: false,
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
